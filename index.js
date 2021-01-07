ad687e8baabfdec5fd0b35219f8be9ed837e61d4
Junaid Tahir <junaidtahir@hotmail.co.uk>
2021-01-07 20:53:05 +0000

    Modal

--- index.js
+++ index.js
@@ -29,3 +29,48 @@ contactBtn.addEventListener("click", function () {
   scroll(contactSection);
 });
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Modal and popup
+
+const modal = document.getElementById("modal");
+const project1Card = document.getElementById("project-card-1");
+const project2Card = document.getElementById("project-card-2");
+const project3Card = document.getElementById("project-card-3");
+const projectCards = document.querySelectorAll(".project-card");
+
+const project1 = document.getElementById("project-1-content");
+const project2 = document.getElementById("project-2-content");
+const project3 = document.getElementById("project-3-content");
+const projects = document.querySelectorAll(".project-content");
+
+const closeModal = () => {
+  modal.style.display = "none";
+  projects.forEach((project) => {
+    project.style.display = "none";
+  });
+};
+
+const openModal = () => {
+  modal.style.display = "block";
+};
+
+const showProject = (e) => {
+  openModal();
+  switch (e.target.id) {
+    case "project-card-1":
+      project1.style.display = "block";
+      break;
+    case "project-card-2":
+      project2.style.display = "block";
+      break;
+    case "project-card-3":
+      project3.style.display = "block";
+      break;
+  }
+};
+
+projectCards.forEach((project) => {
+  project.addEventListener("click", (e) => showProject(e));
+});
+
+modal.addEventListener("click", closeModal);
